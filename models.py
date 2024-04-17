e44569fbc9c32ef5b63b48363134c01d4fd9c67f
FionaG26 <fionamuthoni18@gmail.com>
2024-04-17 19:18:16 +0300

    added postgree username

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from app import app
 
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
