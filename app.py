d351bf3021c8f6ec02dea95cdcb9cabb65204406
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-04-17 18:07:33 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from api.routes import api
+from config import SQLALCHEMY_DATABASE_URI
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Register Blueprint
+app.register_blueprint(api, url_prefix='/api')
+
+if __name__ == "__main__":
+    app.run(debug=True)
