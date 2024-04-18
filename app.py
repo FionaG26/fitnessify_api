b851f767f5c7608ddb75f0b2fd535fb56bd41b79
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-04-18 13:12:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,23 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from api.routes import api
 from config import SQLALCHEMY_DATABASE_URI
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create Swagger blueprint
+SWAGGER_URL = '/api/docs'
+API_URL = '/api/swagger.json'
+swagger_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Fitnessify API Documentation"
+    }
+)
+app.register_blueprint(swagger_blueprint, url_prefix=SWAGGER_URL)
 
 db = SQLAlchemy(app)
 
@@ -14,7 +27,7 @@ app.register_blueprint(api, url_prefix='/api')
 
 @app.route('/')
 def root():
-    return 'Welcome to Fitnessify'
+    return 'Root endpoint'
 
 if __name__ == "__main__":
     app.run(debug=True)
