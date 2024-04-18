0184fc193650cf32d932d4a69ed15fdebce0f7d2
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-04-18 13:23:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from api.routes import api
 from config import SQLALCHEMY_DATABASE_URI
-from flask_swagger_ui import get_swaggerui_blueprint
+from flask_restplus import Api
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Create Swagger blueprint
-SWAGGER_URL = '/api/docs'
-API_URL = '/api/swagger.json'
-swagger_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "Fitnessify API Documentation"
-    }
-)
-app.register_blueprint(swagger_blueprint, url_prefix=SWAGGER_URL)
-
 db = SQLAlchemy(app)
+
+# Create Flask-RESTPlus API instance
+api = Api(app, version='1.0', title='Fitnessify API',
+          description='API documentation for Fitnessify')
 
 # Register Blueprint
 app.register_blueprint(api, url_prefix='/api')
